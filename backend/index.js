021d78af30f424253515eed5b2336559bc7175ca
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-21 10:50:19 +0530

    database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 // const express = require ('express'); method 1
 import express from "express";  //method 2
+import dotenv from "dotenv";
+import connectDB from "./config/database.js"
+dotenv.config({});
 
 const app= express();
 
@@ -7,4 +10,5 @@ const PORT=process.env.PORT || 8080;
 
 app.listen(PORT, ()=>{
     console.log(`server listen at port ${PORT}`);
+    connectDB();
 })
