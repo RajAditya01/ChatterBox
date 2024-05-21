5b300c063e2dac1e7e1d0f810075fc60ad0abf5f
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-21 12:31:33 +0530

    npm i jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 // const express = require ('express'); method 1
 import express from "express";  //method 2
 import dotenv from "dotenv";
-import connectDB from "./config/database.js"
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
 dotenv.config({});
 
 const app= express();
 
 const PORT=process.env.PORT || 8080;
+
+//routes 
+app.use("/api/v1/user", userRoute)
+//
 
 app.listen(PORT, ()=>{
     console.log(`server listen at port ${PORT}`);
