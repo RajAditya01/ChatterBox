3da71497d63827b235375975add52c9d16347ddd
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-21 11:12:28 +0530

    message Schema

---
+++ backend/models/messageModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const messageModel = new mongoose.Schema({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    receiverID:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
+})
+
+export default Message = mongoose.model("Message", messageModel);
